3dc500d4340ecb2e5a6ebbb48da5ed253b2f6b06
Bruno Gomes <brunogomes1198@gmail.com>
2023-07-31 20:32:11 +1000

    css responsive

--- app/static/animations.js
+++ app/static/animations.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
+const enterLinks = document.getElementsByClassName("enter-links")[0];
 toggleBtn.addEventListener("click", function () {
     navbarLinks.classList.toggle("active");
+    enterLinks.classList.toggle("active");
 });
--- create_db.py
+++ create_db.py
@@ -4,6 +4,3 @@ with app.app_context():
     db.create_all()
 
 
-## RENDER.COM
-# db.drop_all()
-# db.create_all()
